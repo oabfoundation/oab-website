f30c08ea1d117207442ec6dd01ec21f77c593e67
Rabeya-Khatun1 <oabfoundation23@gmail.com>
2026-02-23 15:21:32 +0600

    added members, teams and events in navbar and dashboard

---
+++ src/app/(dashboardLayout)/dashboard/projects/page.jsx
@@ -0,0 +1,12 @@
+import Projects from '@/app/(mainLayout)/projects/page';
+import React from 'react';
+
+const DashboardProjects = () => {
+    return (
+        <div>
+            <Projects></Projects>
+        </div>
+    );
+};
+
+export default DashboardProjects;
